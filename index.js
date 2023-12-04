1351a86ebb3af8e1ff95e491e349a65d0409d3a8
AYA-FAYAD <ml.aya.fayad@gmail.com>
2023-12-04 10:33:35 +0300

    add play again btn

--- index.js
+++ index.js
@@ -258,14 +258,16 @@ document.addEventListener('keydown', event => {
 
 
 // win Condition
-
 Events.on(engine, 'collisionStart', event => {
     event.pairs.forEach(collision => {
         const labels = ['ball', 'goal'];
         if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label))
         {
             document.querySelector('.winner').classList.remove('hidden');
-             world.gravity.y = 1;
+            document.querySelector('#btn').addEventListener('click', function() {
+                window.location.reload();
+            })
+            world.gravity.y = 1;
             world.bodies.forEach(body => {
                 if (body.label === 'wall') {
                     Body.setStatic(body, false);
